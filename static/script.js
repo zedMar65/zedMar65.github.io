4e372022a5c1bdbb84b004aa5f34b5f4de0c1e8b
zedMar65 <marius.jedinkus@gmail.com>
2024-05-06 18:21:55 +0300

    birthday

--- static/script.js
+++ static/script.js
@@ -1,3 +1,11 @@
 document.getElementById('scrollButton').addEventListener('click', function() {
     document.getElementById('targetElement').scrollIntoView({ behavior: 'smooth' });
 });
+
+document.getElementById('scrollButton1').addEventListener('click', function() {
+    document.getElementById('targetElement1').scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('scrollButton2').addEventListener('click', function() {
+    document.getElementById('targetElement2').scrollIntoView({ behavior: 'smooth' });
+});
