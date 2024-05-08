162b799be2b4827ac6f194db4f71116c10f194ca
zedMar65 <marius.jedinkus@gmail.com>
2024-05-08 18:59:17 +0300

    Done

--- static/script.js
+++ static/script.js
@@ -9,3 +9,19 @@ document.getElementById('scrollButton1').addEventListener('click', function() {
 document.getElementById('scrollButton2').addEventListener('click', function() {
     document.getElementById('targetElement2').scrollIntoView({ behavior: 'smooth' });
 });
+function copyToClipboard(element) {
+    var textToCopy = element.innerText.trim();
+    
+    navigator.clipboard.writeText(textToCopy).then(function() {
+        var tooltip = document.createElement("span");
+        tooltip.className = "tooltip";
+        tooltip.textContent = "Copied!";
+        element.appendChild(tooltip);
+        
+        setTimeout(function() {
+            tooltip.remove();
+        }, 1000);
+    }, function(err) {
+        console.error('Could not copy text: ', err);
+    });
+}
